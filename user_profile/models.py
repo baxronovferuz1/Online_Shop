49513bc768884b8bda83fd19b1f83314954995d6
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-16 19:00:23 +0500

    commit

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+# target
+# 1)UserInformation
+#     *address
+# 2)UserProfile
+#     *(user,phone_number,address,is_verified,verification_uuid)
+
+
+class UserInformation(models.Model):
+    address=models.CharField(max_length=25)
+
+
+    def __str__(self) -> str:
+        return self.address
+    
+class UserProfile(models.Model):
+    user=models.OneToOneField(User, related_name="users", on_delete=models.CASCADE)
+    phone=models.CharField(max_length=25)
+    address=models.ManyToManyField(UserInformation, related_name="informations", related_query_name="information")
+    is_verified=models.BooleanField
