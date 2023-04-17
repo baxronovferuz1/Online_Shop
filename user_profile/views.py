850cc636933274d8dc699e5e2a357e7c5751720f
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-17 17:39:06 +0500

    wip UserSignUpApiView

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,6 +5,10 @@
 from django.shortcuts import render
 from user_profile import serializers , models
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
 
 class UserSignUPAPIView(APIView):
     serializer_class=serializers.SignUpSerializer
@@ -15,3 +19,5 @@ class UserSignUPAPIView(APIView):
         serializer=serializers.SignUpSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response({'message' : 'You are successfuly signup dear {}'}.format(request.data["username"]))
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
