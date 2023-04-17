ce10223d5016f3c17797d00481aff1d605347c9e
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-17 13:20:34 +0500

    committ

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,9 +1,11 @@
 # target
 # 1)SignUpSerializer(email,phone,password) >> "def create" funtion
-# 2)
+# 2)UpdateProfileSerializer(username,first_name, last_name,email)
+# 3)UpdateProfileRetrieve(Meta)
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from user_profile import models
 
 
 class SignUpSerializer(serializers.Serializer):
@@ -16,3 +18,19 @@ class SignUpSerializer(serializers.Serializer):
         model=User
         fields=("username","email","phone","password")
         write_only_fields=("password" ,) #fielddan kyn "," qo'yilsa keyin typlega aylanadi
+
+
+class UpdateProfileRetrieve(serializers.ModelSerializer):
+
+    class Meta:
+        model=models.UserProfile
+        fields='__all__'
+
+
+class UpdateProfileSerializer(serializers.Serializer):
+    username=serializers.CharField(max_length=40)
+    first_name=serializers.CharField(max_length=50)
+    last_name=serializers.CharField(max_length=50)
+    email=serializers.CharField(max_length=60)
+
+
