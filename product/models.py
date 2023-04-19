42c3df3eab6d55c3d7b161ba410dd9d4563950ed
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-19 23:30:38 +0500

    added BaseItem

--- product/models.py
+++ product/models.py
@@ -8,6 +8,7 @@
 # -image
 # -category
 # -description
+#-price
 # -quantity
 # -created_data
 
@@ -20,6 +21,18 @@ class BaseItem(models.Model):
     category_choise=(("Digital_Product","Digital_Product"),
                      ("Household_appliances","Household_appliances"),
                      ("Educational","Educational"))
+    
+    name=models.CharField(max_length=50)
+    brand=models.CharField(max_length=40)
+    image=models.ImageField(upload_to="Pictutes", null=True, blank=True)
+    category=models.CharField(max_length=100, choices=category_choise)
+    description=models.TextField()
+    price=models.IntegerField()
+    quantity=models.IntegerField()
+    created_data=models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
     
