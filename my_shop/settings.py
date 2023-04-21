7d7de82bc31ea3d308ab58ed199fb32cd7fe46c9
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-21 23:51:55 +0500

    commit

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from PIL import Image
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +43,6 @@ INSTALLED_APPS = [
     'flags',
     'rest_framework',
     'product',
-    'Pillow'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #ACCOUNT_USER_MODEL_USERNAME_FIELD = 'email' 
 
 #ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'
+
