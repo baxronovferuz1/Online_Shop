498d28517f0c53bdabaef3c8d1e57db448895793
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-22 15:22:45 +0500

    commit

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_profile.urls')),
     path('' , include('product.urls')),
+    
 ]
--- product/views.py
+++ product/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet , ModelViewSet , ViewSet
 # Create your views here.
 
-from .models import Television
-from .serializers import TelevisionDetailSerilizer,TelevisionListSerializer
+from .models import Television,Computer,Book,Stationery,Mobile
+from .serializers import *
 from rest_framework import filters
 
 
@@ -20,6 +20,25 @@ class ShowTelevisions(ReadOnlyModelViewSet):
 
     serialzers = {
         'list' : TelevisionListSerializer ,
+        'retrieve' : TelevisionDetailSerilizer
+    }
+
+    def get_serializer_class(self):
+        return self.serialzers.get(self.action)
+    
+
+
+class ShowComputer(ReadOnlyModelViewSet):
+
+    queryset = Computer.objects.all()
+
+    lookup_field = 'slug'
+
+    filter_backends = (filters.SearchFilter , )
+    search_fields = ('name' , 'brand' , 'category')
+
+    serialzers = {
+        'list' : Com,
         'retrieve' : TelevisionDetailSerilizer
     }
 
