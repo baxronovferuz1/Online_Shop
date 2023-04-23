8e3ca2dad8a274d04b750484ca23d036dd6bc9d2
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-23 17:34:00 +0500

    added swagger

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -1,25 +1,30 @@
-"""
-URL configuration for my_shop project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Twitter API',
+        default_version='v1',
+        description='Twitter clone',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_profile.urls')),
     path('' , include('product.urls')),
+    path(
+        "docs/", schema_view.with_ui("swagger", cache_timeout=0), name="docs"
+    ),
+    path(
+        "docs2/", schema_view.with_ui("redoc", cache_timeout=0), name="docs2"
+    )
     
 ]
