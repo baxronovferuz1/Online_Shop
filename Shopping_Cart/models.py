699acf770aa2af0cefc7732eb96947da399de9f9
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-23 23:28:55 +0500

    added status_choice

--- Shopping_Cart/models.py
+++ Shopping_Cart/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import product
 import user_profile
+from product.models import BaseItem
 
 class Shopping_Cart(models.Model):
 
@@ -15,3 +16,11 @@ class Shopping_Cart(models.Model):
         ("on_cart","on_cart"),
         ("ready_to_payment","ready_to_payment")
     )
+
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    item=models.ForeignKey(BaseItem, on_delete=models.CASCADE, null=True)
+    quantity=models.IntegerField(max_length=500)
+    status=models.CharField(max_length=100, choices=status_choice, null=True)
+
+    def __str__(self) -> str:
+        return self.item.name
