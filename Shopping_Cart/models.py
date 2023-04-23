ee5080cda52af21edb16840fb102491912d5753f
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-23 23:20:29 +0500

    added status_choice

--- Shopping_Cart/models.py
+++ Shopping_Cart/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# Target for shopping cart
 
-# Create your models here.
+# 1)create Shopping_cart class
+# status
+
+
+from django.db import models
+from django.contrib.auth.models import User
+import product
+import user_profile
+
+class Shopping_Cart(models.Model):
+
+    status_choice=(
+        ("on_cart","on_cart"),
+        ("ready_to_payment","ready_to_payment")
+    )
