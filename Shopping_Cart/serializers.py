76d38c18f65488d358ff4b1a480fa2ead730ded6
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-26 21:39:34 +0500

    commit

--- Shopping_Cart/serializers.py
+++ Shopping_Cart/serializers.py
@@ -1,5 +1,14 @@
-target for serializers.py
+# target for serializers.py
 
-1)create ShoppingCartSerializer
-2)
+# 1)create ShoppingCartSerializer
+# 2)
 
+
+from rest_framework import serializers
+import user_profile
+import product
+from .models import Shopping_Cart
+
+
+class ShoppingCartSerializer(serializers.Serializer):
+    user = serializers.HiddenField(default = serializers.CurrentUserDefault())
