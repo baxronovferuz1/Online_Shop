bc740ab87e56181b837d351392de45e26ce7f363
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-26 21:48:29 +0500

    wip

--- Shopping_Cart/serializers.py
+++ Shopping_Cart/serializers.py
@@ -6,9 +6,17 @@
 
 from rest_framework import serializers
 import user_profile
-import product
+from product.models import BaseItem
 from .models import Shopping_Cart
 
 
 class ShoppingCartSerializer(serializers.Serializer):
     user = serializers.HiddenField(default = serializers.CurrentUserDefault())
+    item=serializers.PrimaryKeyRelatedField(queryset=BaseItem.objects.all())
+    item_name=serializers.SerializerMethodField("item_name_function")
+
+
+
+    def item_name_function(self, obj):
+        return obj.item.name
+
