b84aed288a89a893ddf7aa55e60e8a91895d7476
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-29 22:56:11 +0500

    wip

--- ticket_calendar/views.py
+++ ticket_calendar/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import CalendarSerializer
+from .models import Calendar
 # Create your views here.
 
 import datetime
 
-current_date = datetime.date.today()
+
+
+class CalendarViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class=CalendarSerializer
+
+    current_date = datetime.date.today()
+
+    def check_date(self):
+        queryset=Calendar.get(day=consert_date)
+
+        if current_date==queryset:
+            return Calendar
+        else:
+            return
+
+    
 
 
