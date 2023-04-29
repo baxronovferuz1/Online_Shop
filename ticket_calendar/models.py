0b3ec85b9df3b0d75515ce9cc9f065f2bc9c5480
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-29 18:22:43 +0500

    commit

--- ticket_calendar/models.py
+++ ticket_calendar/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from events.models import Event, EventSession, Event, EventImage
 
 
 class Calendar(models.Model):
-    user_data=models.ForeignKey()
+    user_data=models.ForeignKey(Event, on_delete=models.CASCADE, null=False)
+    consert_date=models.ForeignKey(EventSession, on_delete=models.CASCADE, null=False)
+    corsert_image=models.ForeignKey(EventImage, on_delete=models.CASCADE, null=False)
+
+
+    def __str__(self) -> str:
+        return self.consert_image.image
