5dc9a57af24f3141dbd68f002e6058bdad1198ab
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-04-30 15:02:41 +0500

    commit

--- ticket_calendar/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Calendar
-
-
-class CalendarSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model=Calendar
-        fields="__all__"
