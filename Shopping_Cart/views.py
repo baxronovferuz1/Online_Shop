0d9e2cafe05cf9ee0b62bae137633c6dcb0cea23
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-02 22:59:49 +0500

    added My_Cart

--- Shopping_Cart/views.py
+++ Shopping_Cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#target for view.py
+# 1)create My_Cart class
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Shopping_Cart
+
+
+class My_Cart(ModelViewSet):
+
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        return Shopping_Cart.objects.filter(user=self.request.user, status="on_cart")
