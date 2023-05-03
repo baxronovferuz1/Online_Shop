5a27285c56c987a86ea34734038d41199d8503a5
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-03 15:59:22 +0500

    add create

--- Shopping_Cart/views.py
+++ Shopping_Cart/views.py
@@ -9,6 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Shopping_Cart
 from .serializers import *
+from rest_framework.response import Response
 
 
 class My_Cart(ModelViewSet):
@@ -36,3 +37,7 @@ class My_Orders(ModelViewSet):
     
     def get_serializer_class(self):
         return self.serializer.get(self.action)
+    
+    
+    def create(self, request, *args, **kwargs):
+        return Response("Tasdiqlash yoki rad etish uchun birorta bo'limni tanlang")
