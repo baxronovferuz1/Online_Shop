febac67b224acb10e940a1097f737d8821c4bcfa
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-03 15:01:50 +0500

    commit

--- Shopping_Cart/views.py
+++ Shopping_Cart/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Shopping_Cart
-from .serializers import ShoppingCartSerializer
+from .serializers import *
 
 
 class My_Cart(ModelViewSet):
@@ -19,3 +19,18 @@ class My_Cart(ModelViewSet):
         return Shopping_Cart.objects.filter(user=self.request.user, status="on_cart")
     
     serializer_class=ShoppingCartSerializer
+
+
+class My_Orders(ModelViewSet):
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        return Shopping_Cart.objects.filter(user=self.request.user, status="on_cart")
+    
+    serializer={
+        'list':ItemInOrderList,
+        'retrieve':ItemInOrderDetail,
+        'create':Go_To_Confirmation_Step,
+        'update':Confirmation,
+    }
+    
