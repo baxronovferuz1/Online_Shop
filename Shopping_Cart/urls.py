7b81c918dca2d872905aa3e994c02e2db89c7ed2
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-07 17:45:20 +0500

    added urls

---
+++ Shopping_Cart/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+
+from .views import My_Cart,Payment,My_Orders
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+
+router.register("my_cart", My_Cart, basename="My_Cart")
+router.register("my_orders", My_Orders, basename="My_Orders")
+router.register("payment", Payment, basename="Payment")
+
+urlpattern={
+    path("finance/", include(router.urls)),
+}
