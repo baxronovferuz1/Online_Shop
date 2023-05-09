ec432d2938abe99af1f88cfd808d9a7a107e522f
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-09 17:37:51 +0500

    added Factor

--- suppliar/views.py
+++ suppliar/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from .models import Suppliar_Check
+from .serializers import Suppliar_Factor,CheckOrdersDetail
+
+class Factor(ModelViewSet):
+    permission_classes=[IsAdminUser]
+    queryset=Suppliar_Check.objects.filter(status='')
+
+    def get_serializer_class(self):
+        if self.action=="list":
+            return Suppliar_Factor
+        else:
+            return CheckOrdersDetail
+        
+    
+
