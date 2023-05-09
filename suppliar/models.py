fd0b422ec64459b249c1eb31c2beb24fa72b7c8b
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-09 11:18:28 +0500

    added Suppliar_Check

--- suppliar/models.py
+++ suppliar/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user_profile.models import UserInformation, User
+from Shopping_Cart.models import Shopping_Cart
 
 # Create your models here.
+
+# target
+
+# create Class Suppliar check(yetkazib berishni tekshiradi)
+# -status
+
+# -phone number
+# -status
+# -addres
+# -recipient(qabul qiluvchi)
+
+
+class Suppliar_Check(models.Model):
+
+    status_choices=(
+        ("ready","Ready"),
+        ("on_process","on_procces"),
+    )
+
+    phone_n=models.CharField(max_length=13)
+    status=models.CharField(max_length=20, choices=status_choices)
+    addres=models.ForeignKey(UserInformation, on_delete=models.DO_NOTHING)
+    recipient=models.ForeignKey(User, on_delete=models.CASCADE)
+    factor=models.ForeignKey(Shopping_Cart, on_delete=models.CASCADE, null=True, blank=True)
+
+
