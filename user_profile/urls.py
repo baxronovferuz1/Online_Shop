70580cf555cebc5dbf704d71460de8d8c7085138
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-10 19:30:00 +0500

    wip

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -3,11 +3,14 @@ from django.urls import path , include
 
 from rest_framework.routers import DefaultRouter
 
-from user_profile import views
+from user_profile.views import *
 
 router = DefaultRouter()
-router.register('update_profile' , views.UpdateProfileView , basename = 'userprofile')
+router.register("update_profile" , UpdateProfileView , basename = 'userprofile')
+router.register("shopping_cart", ShowMyShoppingCartView, basename="myshopping_cart")
+
 
 urlpatterns=[
-    path("signup/", views.UserSignUPAPIView.as_view())
+    path("signup/", UserSignUPAPIView.as_view()),
+    path("edit_profile", include(router.urls)),
 ]
