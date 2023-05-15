315fe3c0559d97f72daa5f15d47a64d3588eb4db
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-15 16:06:53 +0500

    added ItemsAdmin

--- Shopping_Cart/admin.py
+++ Shopping_Cart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Shopping_Cart
+
+admin.site.register(Shopping_Cart)
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+class ItemsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(BaseItem, ItemsAdmin)
+admin.site.register(Television , ItemsAdmin)
+admin.site.register(Computer , ItemsAdmin)
+admin.site.register(Mobile , ItemsAdmin)
+admin.site.register(Book , ItemsAdmin)
+admin.site.register(Stationery , ItemsAdmin)
