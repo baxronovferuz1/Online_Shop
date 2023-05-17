8709d8eec92336d994cd95bd717e079b7a103861
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-17 17:06:54 +0500

    changed setting

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['ENV']=='dev'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com','192.168.1.155']
 
 
 # Application definition
