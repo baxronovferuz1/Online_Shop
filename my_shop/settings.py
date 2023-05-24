2e9512dce20d962ac74d29b7f89bc67c3d9b22d0
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-05-24 14:28:45 +0500

    removed Py-Jwt

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['ENV']=='dev'
 
-ALLOWED_HOSTS = ['.pythonanywhere.com','192.168.1.155']
+ALLOWED_HOSTS = ['.pythonanywhere.com','192.168.1.155', 'localhost']
 
 
 # Application definition
